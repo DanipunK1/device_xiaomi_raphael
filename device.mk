8710d5545df9f8780bb424c15d5f4bf063d66bae
dataoutputstream <mb150996@gmail.com>
2022-01-07 08:58:09 +0200

    raphael: Build audio.primary.default and audio.primary.msmnile
    *Keep quiet log

--- device.mk
+++ device.mk
@@ -62,6 +62,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio.primary.default \
+    audio.primary.msmnile \
     audio.usb.default \
     audio.r_submix.default
 
