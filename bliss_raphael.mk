9489e7c04d58c5106881f04d5f14f9b9e3ae7fdc
DanipunK1 <pegate.eltrueco@gmail.com>
2022-05-18 18:42:35 +0200

    raphael: Build basic gapps
    https://gitlab.com/crdroidandroid/android_vendor_gapps

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -17,6 +17,8 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
+$(call inherit-product, vendor/gapps/basic/config.mk)
+
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
