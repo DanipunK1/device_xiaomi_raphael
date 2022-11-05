df67c5d431b04395536cc37a0feb3abd00e25927
DanipunK1 <pegate.eltrueco@gmail.com>
2022-11-05 20:53:37 +0100

    raphael: remove Aperture

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    Aperture \
     RecorderPrebuilt \
     SafetyHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
