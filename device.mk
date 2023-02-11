a5c881ef4697fec6fe2a3a8c9f58d6b9baeac0b8
DanipunK1 <pegate.eltrueco@gmail.com>
2023-02-11 14:51:36 +0100

    Revert "raphael: shim the GetPreviewImageData symbol on libpiex"
    This reverts commit adedc7b49d46814f5f191e3637f92c6cbdff9265.

--- device.mk
+++ device.mk
@@ -121,7 +121,6 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor \
     libdng_sdk.vendor \
     libshim_megvii \
-    libpiex_shim \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
